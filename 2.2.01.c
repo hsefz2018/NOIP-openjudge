939e56c0936bcc9e14f6104fe138ac8f368ce2b0
Pisces000221 <1786762946@qq.com>
2015-08-21 14:53:39 +0800

    Create 2.2.01.c

---
+++ 2.2.01.c
@@ -0,0 +1,30 @@
+// Submission #3365787
+#include <stdio.h>
+#define MAXN 8
+#define TRUE  1
+#define FALSE 0
+
+int len;
+unsigned char used[MAXN] = { FALSE };
+char s[MAXN], generated[MAXN];
+void dfs(int depth)
+{
+    if (depth == len) puts(generated);
+    else {
+        int i;
+        for (i = 0; i < len; ++i) if (!used[i]) {
+            used[i] = TRUE;
+            generated[depth] = s[i];
+            dfs(depth + 1);
+            used[i] = FALSE;
+        }
+    }
+}
+
+int main()
+{
+    gets(s);
+    len = strlen(s);
+    dfs(0);
+    return 0;
+}
