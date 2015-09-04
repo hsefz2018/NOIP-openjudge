afcd3865837f7bd36f28a70a0b714833908b5f0c
PatrickW <magetron1@gmail.com>
2015-09-04 23:09:46 +0800

    Quick Solv

---
+++ 1.3.12.pas
@@ -0,0 +1,5 @@
+var r:extended;
+begin
+	readln(r);
+	writeln(r*r*r*3.14*4/3:0:2);
+end.
---
+++ 1.3.13.pas
@@ -0,0 +1,6 @@
+var str:string;
+	i:longint;
+begin
+	readln(str);
+	for i:=length(str) downto 1 do write(str[i]);
+end.
