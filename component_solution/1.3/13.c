238f1cccaa7485bf0076b51968147ebd10c307d3
Pisces000221 <1786762946@qq.com>
2016-04-28 14:23:53 +0800

    Create 13.c

---
+++ component_solution/1.3/13.c
@@ -0,0 +1,13 @@
+// Submission #5301728
+#include <stdio.h>
+
+int main()
+{
+    char a = getchar();
+    char b = getchar();
+    putchar(getchar());
+    putchar(b);
+    putchar(a);
+    putchar('\n');
+    return 0;
+}
